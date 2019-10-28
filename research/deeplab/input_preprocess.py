b38d33ecb3d6451e66b44402cb8bb2ba9f65b61d
Rishab Sharma <rishusharma.sharma7@gmail.com>
2019-10-28 18:10:26 +0530

    Update input_preprocess.py

--- research/deeplab/input_preprocess.py
+++ research/deeplab/input_preprocess.py
@@ -127,11 +127,11 @@ def preprocess_image_and_label(image,
   else:
     rr = tf.minimum(tf.cast(crop_height,tf.float32)/tf.cast(image_height,tf.float32),\
              tf.cast(crop_width,tf.float32)/tf.cast(image_width,tf.float32))
-     newh = tf.cast(tf.cast(image_height, tf.float32)*rr, tf.int32) 
-     neww = tf.cast((tf.cast(image_width, tf.float32)*rr), tf.int32)
-     processed_image = tf.image.resize_images(
+    newh = tf.cast(tf.cast(image_height, tf.float32)*rr, tf.int32) 
+    neww = tf.cast((tf.cast(image_width, tf.float32)*rr), tf.int32)
+    processed_image = tf.image.resize_images(
          processed_image, (newh, neww), method=tf.image.ResizeMethod.BILINEAR, align_corners=True)
-     processed_image = preprocess_utils.pad_to_bounding_box(
+    processed_image = preprocess_utils.pad_to_bounding_box(
                          processed_image, 0, 0, crop_height, crop_width, mean_pixel)
 
   processed_image.set_shape([crop_height, crop_width, 3])
